0bce66a661bd322a701062a73d5c900f73b0c34c
Ch-Hanuman <chinthapalli.hanuman7@gmail.com>
2025-07-26 20:01:14 +0530

    Initial Application

---
+++ src/main/java/com/hostel/management_prototype/ManagementPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.hostel.management_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostel/management_prototype/ManagementPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostel.management_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
